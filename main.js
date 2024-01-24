e977f8e17e219ed729712773b1fae1282eb3997f
Niakrist <ak_88dron@inbox.ru>
2024-01-24 19:40:17 +0300

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,6 @@ const main = async (page) => {
 
   const response = await fetch(`https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${page}`)
   console.log('loader 1')
-  preloader.style.display = "none"
   const data = await response.json();
   console.log(data)
   data.forEach(cart => {
@@ -31,6 +30,7 @@ const main = async (page) => {
     `;
     console.log(cartEl)
     wrapper.appendChild(cartEl)
+    preloader.style.display = "none";
   })
 
   const lastPage = document.querySelector('.cart:last-child');
